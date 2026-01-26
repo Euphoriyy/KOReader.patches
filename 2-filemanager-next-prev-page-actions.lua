949f10eccfeb66a92d12a1bb65f0eb957efbf780
Euphoriyy <jstkidding@mail.com>
2026-01-26 02:02:25 -0500

    feat(filemanager-actions): update patch

--- 2-filemanager-next-prev-page-actions.lua
+++ 2-filemanager-next-prev-page-actions.lua
@@ -1,4 +1,4 @@
--- User patch to add dispatcher actions for going to the next/prev page in filemanager
+-- User patch to add dispatcher actions for going to the next & previous page in filemanager
 -- Priority: 2
 
 local Dispatcher = require("dispatcher")
@@ -6,7 +6,7 @@ local FileManager = require("apps/filemanager/filemanager")
 local _ = require("gettext")
 
 -- Add the onGotoNextPage method to FileManager
-local FileManager_onGotoNextPage = function(self)
+FileManager.onGotoNextPage = function(self)
     if self.file_chooser then
         local current_page = self.file_chooser.page
         local total_pages = self.file_chooser.page_num
@@ -21,10 +21,8 @@ local FileManager_onGotoNextPage = function(self)
     return false
 end
 
-FileManager.onGotoNextPage = FileManager_onGotoNextPage
-
 -- Add the onGotoPrevPage method to FileManager
-local FileManager_onGotoPrevPage = function(self)
+FileManager.onGotoPrevPage = function(self)
     if self.file_chooser then
         local current_page = self.file_chooser.page
 
@@ -38,19 +36,17 @@ local FileManager_onGotoPrevPage = function(self)
     return false
 end
 
-FileManager.onGotoPrevPage = FileManager_onGotoPrevPage
-
 -- Register the dispatcher actions
 Dispatcher:registerAction("filemanager_next_page", {
     category = "none",
     event = "GotoNextPage",
-    title = _("Next page in file browser"),
+    title = _("Next Page"),
     filemanager = true,
 })
 
 Dispatcher:registerAction("filemanager_prev_page", {
     category = "none",
     event = "GotoPrevPage",
-    title = _("Previous page in file browser"),
+    title = _("Previous Page"),
     filemanager = true,
 })
